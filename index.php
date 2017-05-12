2d588fc82875780535a9a0b63df577abdee56b91
renan <renan@DESKTOP-N809Q4U>
2017-05-11 23:01:00 -0300

    Projeto ok

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso de HTML5</title>
+        <meta name="Description" content="Esta pagina web esta sendo construida em HTML5"/>
+        <meta name="viewport" content="width-width, initial-scale=1.0"/>
+    <!--   
+        FAVICON
+        STYLE
+        FONT
+    -->
+    </head>
+    <body>
+        <header>
+            <img title="Curso de HTML5" alt="[Curso de HTML5]" src="img/_logo.png" />
+            <h1>Curs de HTML5 - Upinside Treinametos</h1>
+            <ul>
+                <li><a href="" title="">HOME</a></li>
+            </ul>
+        </header>
+        
+        <main>
+            
+        </main>
+        
+        <footer>
+            
+        </footer>
+    </body>
+</html>
